c81f58442c5b21db4339c10603e070aca5e8c956
Tejas Nampalli <TSNampallI@gmail.com>
2024-03-15 11:29:53 -0400

    finished

---
+++ src/components/Tile.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+
+function Tile({name, specialty, greased, weight, highestMedal, image}) {
+
+    const [isClicked, setIsClicked] = useState(false)
+
+    function handleClick(e) {
+        setIsClicked(!isClicked)
+    }
+
+return (
+<div onClick={handleClick}>
+  <div>{name}</div>
+  <div>{isClicked ? specialty : null}</div>
+  <div>{isClicked ? greased : null}</div>
+  <div>{isClicked ? weight : null}</div>
+  <div>{isClicked ? highestMedal : null}</div>
+  <img src={image} alt={name} />  
+</div>
+)
+
+}
+
+
+export default Tile;
